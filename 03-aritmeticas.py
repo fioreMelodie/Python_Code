69765b8092f707309148d2332285a47b2dff7e6c
fioreMelodie <adrianapsanchez27@gmail.com>
2024-07-05 22:04:26 -0500

    Se agregan operaciones aritméticas de python

---
+++ 03-aritmeticas.py
@@ -0,0 +1,7 @@
+#Operaciones Aritméticas
+print(5 + 6) # Para realizar sumas 
+print(10 - 8) # Para realizar restas 
+print(5 * 6) # Para realizar multiplicaciones 
+print(25 / 5) # Para realizar divisiones 
+print(5 ** 2) # Para elevar números
+print(20 % 6) # Para sacar el módulo de un numero (cuantas veces cabe a(6) entre b(20)) y lo que sobre es el módulo

492dc7e8cbeeed0d79fd9a4e5ea5f9276b350e38
fioreMelodie <adrianapsanchez27@gmail.com>
2024-07-05 22:31:14 -0500

    Pedirle información al usuario mediante el "input" y ver el tipo de dato que maneja una variable con el "type"

---
+++ 05-input.py
@@ -0,0 +1,5 @@
+resultado =input("Ingresa tu edad: ") #Permite obtener datos del usuario (llamarlos)   
+#y el resultado se registra en la variable
+
+print(type(resultado)) #Permite ver el tipo de dato
+print("Su edad es:",resultado)

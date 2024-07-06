da32a8547f4641b18955a73ab0704472f96e2fb0
fioreMelodie <adrianapsanchez27@gmail.com>
2024-07-05 21:43:38 -0500

    Tipos de strings y ejemplos de como se utilizan

---
+++ 02-strings.py
@@ -0,0 +1,8 @@
+texto = "Hola Mundo"
+print(texto.upper())
+print(texto.lower())
+print(texto.find("Mun"))
+nuevotexto = texto.replace("Mun","Fernan")
+print(texto,nuevotexto)
+
+print("Mundo" in texto)

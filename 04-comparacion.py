bfb4f4120b3aee6bf683bdb11e33e5eb05195a8f
fioreMelodie <adrianapsanchez27@gmail.com>
2024-07-05 22:11:44 -0500

    Se agregan operadores de comparación con resultados boleanos

---
+++ 04-comparacion.py
@@ -0,0 +1,11 @@
+#Operadores de comparación
+n1 = 10
+n2= 15
+print(n1 > n2) #Mayor que
+print(n1 < n2) #Menor que
+print(n1 <= 10) #Menor o igual que
+print(n1 >= 10) #Mayor o igual que
+print(n2 == 10) #Mayor o igual que
+
+# Devuelve valores boleanos
+# Sirven para realizar validaciones

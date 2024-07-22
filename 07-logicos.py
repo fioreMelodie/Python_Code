991fa81a1998a99425741a2f6ed2020e16bfadb7
fioreMelodie <adrianapsanchez27@gmail.com>
2024-07-22 16:52:24 -0500

    Se agregan operadores lógicos básicos

---
+++ 07-logicos.py
@@ -0,0 +1,11 @@
+edad = 22
+print(edad > 18 and edad < 30)
+# El operador lógico "and" funciona  de izquiera a derecha (basta con que uno de los dos de como
+# resultado falso para que el resultado final sea False) 
+
+print(edad > 18 or edad < 30)
+# El operador lógico "OR" tiene como fin el ejecutar las dos sentencias y devolver un valor de "True"
+
+print(not (edad < 17))
+#El operador "NOT" tiene como función el negar loq eu se le asigne a la derecha de este entre paréntesis
+# si es "True" lo pasa a "False" y viceversa  
